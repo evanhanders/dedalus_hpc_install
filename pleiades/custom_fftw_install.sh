326f6c1381c3778cc78e4c88acc36587d78ae5e3
Evan H. Anders <evanhanders@gmail.com>
2022-08-24 08:21:05 -0700

    updates fftw install with some useful links, new options, cleans it up a bit.

--- pleiades/custom_fftw_install.sh
+++ pleiades/custom_fftw_install.sh
@@ -1,9 +1,13 @@
-# Code from Ben Brown for installing FFTW on pleiades
+# Code from Ben Brown modified by Evan Anders for installing FFTW on pleiades
+# Info on NASA intel compilers: https://www.nas.nasa.gov/hecc/support/kb/intel-compiler_86.html
+# Info on NASA mpt: https://www.nas.nasa.gov/hecc/support/kb/porting-with-hpe-mpt_100.html
+# Info on an mpt alternative: https://www.nas.nasa.gov/hecc/support/kb/hpc-x-mpi-an-alternative-to-mpt_619.html
+# Recommended compilation flags: https://www.nas.nasa.gov/hecc/support/kb/recommended-compiler-options_99.html
 
-FFTW_PATH="/swbuild/eanders/conda_install/fftw-3.3.9"
+FFTW_PATH="`pwd`/fftw-3.3.9"
 pwd
 
-DEST_SUFFIX="fftw_install_new"
+DEST_SUFFIX="fftw_install"
 DEST_DIR="`pwd`/${DEST_SUFFIX/ /}"
 
 if [ -e $FFTW_PATH ]
@@ -28,40 +32,30 @@ else
     MPICXX="mpicxx"
     MPIF90="mpif90"
 
-    ARCH_CONF="-axCORE-AVX512 -xSSE4.2"
-    FFTW_FLAGS="-O2 "$ARCH_CONF" -fPIC"
+    #ARCH_CONF may actually mess stuff up on rome nodes: https://www.nas.nasa.gov/hecc/support/kb/preparing-to-run-on-aitken-rome-nodes_657.html
+    ARCH_CONF="-axCORE-AVX512,CORE-AVX2 -xAVX"
+    FFTW_FLAGS="-O3 "$ARCH_CONF" -fPIC -w -qopenmp -parallel"
 
-    #Pleiades configure args
-    #--enable-shared --enable-threads --prefix=/nasa/pkgsrc/toss3/2021Q2 
-    #--build=x86_64-redhat-linux --host=x86_64-redhat-linux 
-    #--infodir=/nasa/pkgsrc/toss3/2021Q2/info --mandir=/nasa/pkgsrc/toss3/2021Q2/man 
-    #--enable-option-checking=yes
-
-    #Pleiades flags
-    #    CPPFLAGS=   -I/usr/include -I/nasa/pkgsrc/toss3/2021Q2/include
-    #    FFLAGS=-O
-    #    CFLAGS=-O2 -pipe -D_FORTIFY_SOURCE=2 -I/usr/include -I/nasa/pkgsrc/toss3/2021Q2/include
-    #    LDFLAGS=   -L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -L/usr/lib64 -Wl,-R/usr/lib64 -L/nasa/pkgsrc/toss3/2021Q2/lib -Wl,-R/nasa/pkgsrc/toss3/2021Q2/lib
-
+    #possibly useful: --enable-avx512 --enable-avx2 --enable-avx 
 
     FFTWCONF_ARGS="CC="$MPICC" \
                    CXX="$MPICXX" \
                    F77="$MPIF90" \
                    MPICC="$MPICC" MPICXX="$MPICXX" \
-                   --enable-shared --enable-avx512 \
+                   --enable-shared\
                    --enable-mpi --enable-openmp --enable-threads \
                    --build=x86_64-redhat-linux --host=x86_64-redhat-linux \
                    --enable-option-checking=yes"
-    CPPFLAGS=$FFTW_FLAGS" -I/usr/include -I/nasa/pkgsrc/toss3/2021Q2/include"
+    CPPFLAGS=$FFTW_FLAGS
     FFLAGS=$FFTW_FLAGS
-    CFLAGS=$FFTW_FLAGS" -I/usr/include -I/nasa/pkgsrc/toss3/2021Q2/include"
+    CFLAGS=$FFTW_FLAGS
     echo $CFLAGS
-    LDFLAGS="-L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -L/usr/lib64 -Wl,-R/usr/lib64 -L/nasa/pkgsrc/toss3/2021Q2/lib -Wl,-R/nasa/pkgsrc/toss3/2021Q2/lib"
+#    LDFLAGS="-L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib/gcc/x86_64-redhat-linux/7.5.0 -L/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -Wl,-R/nasa/pkgsrc/toss3/2021Q2/gcc7/lib64 -L/usr/lib64 -Wl,-R/usr/lib64 -L/nasa/pkgsrc/toss3/2021Q2/lib -Wl,-R/nasa/pkgsrc/toss3/2021Q2/lib"
+#                LDFLAGS="$LDFLAGS" \
 
     ./configure --prefix=${DEST_DIR}/ ${FFTWCONF_ARGS} \
                 CFLAGS="$CFLAGS" \
                 CPPFLAGS="$CPPFLAGS" \
-                LDFLAGS="$LDFLAGS" \
                 FFLAGS="$FFLAGS" \
                 F90
 
